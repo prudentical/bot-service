a3adb37b368c1594b9c1682f40a1cf5022973673
Alireza Dastyar <alireza@dastyar.space>
2025-05-12 11:02:19 +0000

    Extract the getPositionGrid method to the grider

--- src/main/java/com/prudentical/botservice/service/bot/grid/Grider.java
+++ src/main/java/com/prudentical/botservice/service/bot/grid/Grider.java
@@ -2,12 +2,17 @@ package com.prudentical.botservice.service.bot.grid;
 
 import java.util.Map;
 import java.util.Optional;
+
+import com.prudentical.botservice.dto.PositionDTO;
+
 import java.math.BigDecimal;
 
 public interface Grider {
 
-    Map<Integer, BigDecimal> getGridPricePoints(BigDecimal ceiling, BigDecimal floor, int grids);
+    Map<Integer, BigDecimal> getGridPricePoints(BigDecimal floor, BigDecimal ceiling, int grids);
 
     Optional<Integer> getPriceGrid(Map<Integer, BigDecimal> gridPricePoints, BigDecimal price);
+
+    int getPositionGrid(Map<Integer, BigDecimal> gridPricePoints, PositionDTO position);
 
 }
